a51f56eb43c40862ee473bc4979c82d763615eec
Pete Lower <pete.lower@gmail.com>
2013-07-28 14:27:39 -0500

    Syntax error fix

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
+<?php
+
 function lowermedia_add_admin_favicon() {
 	$favicon_url = get_stylesheet_directory_uri().'/favicon.ico';
 	echo '<link rel="shortcut icon" href="'.$favicon_url .'" />';
 }
 add_action('admin_head', 'lowermedia_add_admin_favicon');
+
+?>
