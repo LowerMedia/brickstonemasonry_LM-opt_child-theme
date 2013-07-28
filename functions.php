f7f2efa37cad7f8eb765e18cb124e198bb43f690
Pete Lower <pete.lower@gmail.com>
2013-07-28 14:30:01 -0500

    Adjusting function to add to both public and admin head

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function lowermedia_add_admin_favicon() {
 	$favicon_url = get_stylesheet_directory_uri().'/favicon.ico';
 	echo '<link rel="shortcut icon" href="'.$favicon_url .'" />';
 }
-add_action('admin_head', 'lowermedia_add_admin_favicon');
+add_action('admin_head', 'lowermedia_add_favicon');
+add_action('wp_head', 'lowermedia_add_favicon');
 
 ?>
